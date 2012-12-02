094154152e98f99886b12e50a1482ffec784aa0f
Craig McQueen <craig@mcqueen.id.au>
2012-12-02 14:35:53 +1100

    Refactor.

--- format-preserving-encryption.py
+++ format-preserving-encryption.py
@@ -6,39 +6,66 @@ import struct
 # 3rd-party modules
 from Crypto.Cipher import AES
 
-def fpe2encrypt(message, key, rounds=10, message_width=32, decrypt=False):
-    '''
-    message is an integer.
-    key is a 16-byte string
-    '''
-    widths = [0, 0]
-    widths[0] = (message_width + 1) // 2
-    widths[1] = message_width - widths[0]
+class FPE2:
+    def __init__(self, key, rounds=10, message_width=32):
+        '''
+        key is a 16- 24- or 32-byte string
+        '''
+        self.key = key
+        self.aes_obj = AES.new(key, AES.MODE_ECB)
+        self.rounds = rounds
+        widths = [(message_width + 1) // 2, message_width // 2]
+        modulos = [ 1 << width for width in widths ]
+        masks = [ modulo - 1 for modulo in modulos ]
+        self.message_width = message_width
+        self.widths = widths
+        self.modulos = modulos
+        self.masks = masks
 
-    # Split message into working parts
-    work = [0, 0]
-    work[0] = message & ((1 << widths[0]) - 1)
-    message >>= widths[0]
-    work[1] = message & ((1 << widths[1]) - 1)
+    def split_message(self, message):
+        '''Split message into working parts'''
+        work = [0, 0]
+        work[0] = message & self.masks[0]
+        message >>= self.widths[0]
+        work[1] = message & self.masks[1]
+        return work
 
-    aes_obj = AES.new(key, AES.MODE_ECB)
+    def encrypt(self, message):
+        '''
+        message is an integer.
+        '''
+        work = self.split_message(message)
+        for round in range(self.rounds):
+            i_from = round % 2
+            i_to = (round + 1) % 2
+            byte_data = struct.pack("<QQ", round, work[i_from])
+            encrypt_data = self.aes_obj.encrypt(byte_data)
+            temp, = struct.unpack("<8xQ", encrypt_data)
+            temp %= (1 << self.widths[i_to])
+            work[i_to] = (work[i_to] + temp) % (1 << self.widths[i_to])
+        return (work[1] << self.widths[0]) | work[0]
 
-    for round in (range(rounds) if not decrypt else range(rounds-1, -1, -1)):
-        i_from = round % 2
-        i_to = (round + 1) % 2
-        byte_data = struct.pack("<QQ", round, work[i_from])
-        encrypt_data = aes_obj.encrypt(byte_data)
-        temp, = struct.unpack("<8xQ", encrypt_data)
-        temp %= (1 << widths[i_to])
-        if not decrypt:
-            work[i_to] = (work[i_to] + temp) % (1 << widths[i_to])
-        else:
-            work[i_to] = (work[i_to] - temp) % (1 << widths[i_to])
-    return (work[1] << widths[0]) | work[0]
+    def decrypt(self, message):
+        '''
+        message is an integer.
+        '''
+        work = self.split_message(message)
+        for round in range(self.rounds-1, -1, -1):
+            i_from = round % 2
+            i_to = (round + 1) % 2
+            byte_data = struct.pack("<QQ", round, work[i_from])
+            encrypt_data = self.aes_obj.encrypt(byte_data)
+            temp, = struct.unpack("<8xQ", encrypt_data)
+            temp %= self.modulos[i_to]
+            work[i_to] = (work[i_to] - temp) % self.modulos[i_to]
+        return (work[1] << self.widths[0]) | work[0]
 
 if __name__ == "__main__":
     key = b"testtesttestaaaa"
+    fpe_obj = FPE2(key)
     for i in range(16):
-        encrypted = fpe2encrypt(i, key)
-        decrypted = fpe2encrypt(encrypted, key, decrypt=True)
+        encrypted = fpe_obj.encrypt(i)
+        decrypted = fpe_obj.decrypt(encrypted)
         print("{0:08X} {1:08X} {2:08X}".format(i, encrypted, decrypted))
+        assert (i == decrypted)
+
