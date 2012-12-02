4a0a28d0e4042f08eec545898e9ea49752b33b88
Craig McQueen <craig@mcqueen.id.au>
2012-12-02 23:50:48 +1100

    Update file doc string, including adding MIT license.

--- format-preserving-encryption.py
+++ format-preserving-encryption.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
+"""
+Format-preserving encryption.
+See proposed AES-FFX specs:
+    http://csrc.nist.gov/groups/ST/toolkit/BCM/documents/proposedmodes/ffx/ffx-spec.pdf
+    http://csrc.nist.gov/groups/ST/toolkit/BCM/documents/proposedmodes/ffx/ffx-spec2.pdf
+This uses the overall principles of AES-FFX, method 2. It doesn't conform to the proposed
+FFX-A2, FFX-A10, or FFX[radix] standards.
+
+MIT license:
+
+----------------------------------------------------------------------------
+Copyright (c) 2012 Craig McQueen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+----------------------------------------------------------------------------
+"""
 
 # Standard Python library modules
 import struct
